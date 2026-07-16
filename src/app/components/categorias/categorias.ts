c7e60f83a2314451511ae02a0f4274758f20bf58
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-07-16 00:20:11 -0300

    feat: inicia estrutura de cadastro de categorias no frontend

--- src/app/components/categorias/categorias.ts
+++ src/app/components/categorias/categorias.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Categoria } from '../../models/categoria';
 import { CategoriaService } from './../../services/categoria.service';
 import { Component, OnInit } from '@angular/core';
+import { MdbModalService } from 'mdb-angular-ui-kit/modal';
+import { CategoriaForm } from '../categoria-form/categoria-form';
 
 @Component({
   selector: 'app-categorias',
@@ -10,19 +12,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './categorias.css',
 })
 export class Categorias implements OnInit {
-
   categorias: Categoria[] = [];
 
-  constructor(private categoriaService: CategoriaService){
+  constructor(
+    private modalService: MdbModalService,
+    private categoriaService: CategoriaService,
+  ) {}
 
+  ngOnInit() {
+    this.categoriaService.listar().subscribe({
+      next: (resultado) => {
+        console.log('Resultado', resultado);
+        this.categorias = resultado;
+        console.log('Quantidade', this.categorias.length);
+      },
+      error: (erro) => {
+        console.error('Erro:', erro);
+      },
+    });
   }
 
-  ngOnInit(){
-     console.log("COMPONENTE CATEGORIAS CARREGADO");
-
-    this.categoriaService.listar().subscribe(resultado =>{
-
-        this.categorias = resultado;
-    });
-  } 
+  abrirModal() {
+    this.modalService.open(CategoriaForm);
+  }
 }
